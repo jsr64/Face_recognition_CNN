9b43891d15b84b8d1ff449bb4659816d30f50af8
sarthak7295 <sarthak.rawat@yahoo.com>
2018-08-18 20:44:12 +0530

    sript change for data set creation

--- datasetGenerator.py
+++ datasetGenerator.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import glob
+from PIL import Image
 i = 0
 
 def rotateImage(image, angle):
@@ -10,22 +11,55 @@ def rotateImage(image, angle):
   return result
 
 
+#
+# for filename in glob.iglob('D:\mydataset\positive\*', recursive=True):
+#     img = cv2.imread(filename)
+#     cv2.imwrite("D:\\mydataset\\positive-fin-noreshape\\" + str(i) + ".jpg", img);
+#     i = i + 1
+#     print(i)
+#     rimg = rotateImage(img, 18)
+#     cv2.imwrite("D:\\mydataset\\positive-fin-noreshape\\" + str(i) + ".jpg", rimg);
+#     i = i + 1
+#     rimg2 = rotateImage(img, -18)
+#     print(i)
+#     cv2.imwrite("D:\\mydataset\\positive-fin-noreshape\\" + str(i) + ".jpg", rimg2);
+#     i = i + 1
+#     print(i)
 
-for filename in glob.iglob('D:\lfw\lfw\*\*', recursive=True):
-    img = cv2.imread(filename)
-    rimg = img.copy()
-    rimg1 = rotateImage(rimg, 15)
-    cv2.imwrite("D:\\neg1\\"+str(i)+".jpg", img);
-    i = i+1
-    rimg2 = rotateImage(rimg, -15)
-    cv2.imwrite("D:\\pos1\\" + str(i) + ".jpg", rimg2);
+#
+# for filename in glob.iglob('D:\\mydataset\\negative\\*', recursive=True):
+#     img = cv2.imread(filename)
+#     if(i%4==0):
+#         rimg2 = rotateImage(img, -18)
+#         cv2.imwrite("D:\\mydataset\\negative-fin\\" + str(i) + ".jpg", rimg2)
+#     elif(i%6==0):
+#         rimg = rotateImage(img, 18)
+#         cv2.imwrite("D:\\mydataset\\negative-fin\\" + str(i) + ".jpg", rimg)
+#     else:
+#         cv2.imwrite("D:\\mydataset\\negative-fin\\" + str(i) + ".jpg", img)
+#     print(i)
+#     i = i + 1
+
+#
+#
+# img=cv2.imread('oo.jpg')
+# rimg=img.copy()
+# rimg=cv2.flip(img,1)
+# cv2.imwrite("D:\\pos\\ray_Image.jpg", rimg);
+# abc = rotateImage(img,-10)
+# cv2.imwrite("ray_Image_rotat.jpg", abc);
+#
+# for filename in glob.iglob('D:\mydataset\positive\*', recursive=True):
+#     img = Image.open(filename).convert('L')
+#     img = img.resize((90, 90))
+#     img.save("D:\\mydataset\\positive-fin\\" + str(i) + ".jpg");
+#     i = i + 1
+#     print(i)
+
+
+for filename in glob.iglob('D:\\mydataset\\negative\\*', recursive=True):
+    img = Image.open(filename).convert('L')
+    img = img.resize((90, 90))
+    img.save("D:\\mydataset\\negative-fin\\" + str(i) + ".jpg")
     i = i + 1
-
-
-
-img=cv2.imread('oo.jpg')
-rimg=img.copy()
-rimg=cv2.flip(img,1)
-cv2.imwrite("D:\\pos\\ray_Image.jpg", rimg);
-abc = rotateImage(img,-10)
-cv2.imwrite("ray_Image_rotat.jpg", abc);
+    print(i)
---
+++ random_scripts_test.py
@@ -0,0 +1,40 @@
+import numpy as np
+import cv2
+import glob
+from PIL import Image
+import pandas as pd
+
+img = Image.open('16.jpg')
+image_array = np.asarray(img).astype(int).flatten()                         #.astype(np.float32)
+image_array2 = np.concatenate((np.array([1]),image_array),axis=0).astype(int)
+# dataset = pd.read_csv('train.csv')
+dataset_list =['ans']
+for x in range(8101):
+    dataset_list.append('pixel'+str(x))
+
+
+f=open('train.csv','ab')
+image_array2 = np.concatenate((np.array([1]),image_array),axis=0).astype(np.int16)
+np.savetxt(f, image_array2, delimiter="\\n", newline=',')
+f.write(b'\n')
+
+
+
+
+
+# image_array2 = np.reshape(image_array,(90,90))
+# img2 = Image.fromarray(image_array2)
+# img2 = img.resize((90, 90), Image.ANTIALIAS)
+# img2.save('wow.png')
+# img.save('wowg.png')
+# print(image_array)
+# print(image_array)
+
+
+
+# img.save('16-grey.png')
+# img = Image.open('17.jpg').convert('L')
+# img = img.resize((90, 90), Image.ANTIALIAS)
+# img.save('17-grey.png')
+
+
