f1d83359dcd86fdde861ed937f6b912262cc83be
sarthak7295 <sarthak.rawat@yahoo.com>
2018-08-23 19:08:49 +0530

    dataset generation from small no of files

--- random_scripts_test.py
+++ random_scripts_test.py
@@ -4,22 +4,33 @@ import glob
 from PIL import Image
 import pandas as pd
 
-img = Image.open('16.jpg')
-image_array = np.asarray(img).astype(int).flatten()                         #.astype(np.float32)
-image_array2 = np.concatenate((np.array([1]),image_array),axis=0).astype(int)
+# img = Image.open('16.jpg')
+# image_array = np.asarray(img).astype(int).flatten()                         #.astype(np.float32)
+# image_array2 = np.concatenate((np.array([1]),image_array),axis=0).astype(int)
 # dataset = pd.read_csv('train.csv')
-dataset_list =['ans']
-for x in range(8101):
-    dataset_list.append('pixel'+str(x))
+# dataset_list =['ans']
+# for x in range(8101):
+#     dataset_list.append('pixel'+str(x))
 
 
 f=open('train.csv','ab')
-image_array2 = np.concatenate((np.array([1]),image_array),axis=0).astype(np.int16)
-np.savetxt(f, image_array2, delimiter="\\n", newline=',')
-f.write(b'\n')
+# image_array2 = np.concatenate((np.array([1]),image_array),axis=0).astype(np.int16)
+# np.savetxt(f, image_array2, delimiter="\\n", newline=',')
+# f.write(b'\n')
+# image_array2 = np.concatenate((np.array([1]),image_array),axis=0).astype(np.int16)
+# np.savetxt(f, image_array2, delimiter="\\n", newline=',')
+# f.write(b'\n')
+i=0
 
-
-
+for filename in glob.iglob('D:\\mydataset\\negative-fin\\*', recursive=True):
+    img = Image.open(filename)
+    image_array = np.asarray(img).astype(int).flatten()
+    # image_array2 = np.concatenate((np.array([0]), image_array), axis=0).astype(int)
+    image_array2 = np.concatenate((np.array([0]), image_array), axis=0).astype(np.int16)
+    np.savetxt(f, image_array2, delimiter="\\n", newline=',')
+    f.write(b'\n')
+    i = i + 1
+    print(i)
 
 
 # image_array2 = np.reshape(image_array,(90,90))
