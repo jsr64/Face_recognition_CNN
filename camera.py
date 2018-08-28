c98251529c14a91822b402e56ccd8d1f56253c34
sarthak7295 <sarthak.rawat@yahoo.com>
2018-08-28 13:35:42 +0530

    done

--- camera.py
+++ camera.py
@@ -1,36 +1,27 @@
 from darkflow.net.build import TFNet
 import cv2
 from PIL import Image
+# #
+# # options = {"model": "cfg/tiny-yolo-voc.cfg", "load": "bin/yolov2-tiny-voc.weights", "threshold": 0.2}
+# #
+# # tfnet = TFNet(options)
+# #
+# # imgcv = cv2.imread("test.jpg")
+# # result = tfnet.return_predict(imgcv)
+# # # print(type(result))
+# # # print(type(result[0]))
+# # print(result)
+# # print(imgcv.shape[0])   #height widhth channel
+# # print(result[0]["label"])
+#
+# cv2.imwrite('test-result.png',img)
+# img = Image.open('test-result.png').convert('L')
+# img = img.resize((90, 90))
+# img.save('test-result.png')
 
-options = {"model": "cfg/tiny-yolo-voc.cfg", "load": "bin/yolov2-tiny-voc.weights", "threshold": 0.2}
-
-tfnet = TFNet(options)
-
-imgcv = cv2.imread("1.jpg")
-result = tfnet.return_predict(imgcv)
-# print(type(result))
-# print(type(result[0]))
-print(result)
-print(imgcv.shape[0])   #height widhth channel
-print(result[0]["label"])
-i=0
-for person in result:
-    topleft_x = person['topleft']['x']
-    topleft_y = person['topleft']['y']
-    bottomright_x = person['bottomright']['x']
-    bottomright_y = person['bottomright']['y']
-    if((topleft_y+imgcv.shape[0]*0.08)>imgcv.shape[0]):
-        topleft_y = int(imgcv.shape[0])
-    else:
-        topleft_y = int(topleft_y - (imgcv.shape[0] * 0.1))
-    bottomright_y = int(bottomright_y - (imgcv.shape[0] * 0.1))
-    img = cv2.rectangle(imgcv, (topleft_x, topleft_y), (bottomright_x, bottomright_y), (0, 255, 0), 5)
-    crop_img = img[topleft_y:bottomright_y, topleft_x:bottomright_x]
-    cv2.imwrite('test-result'+str(i)+'.png', crop_img)
-    i=i+1
-
-cv2.imwrite('test-result.png',img)
-img = Image.open('test-result.png').convert('L')
-img = img.resize((90, 90))
-img.save('test-result.png')
-
+def extract_result_set(file_name):
+    options = {"model": "cfg/tiny-yolo-voc.cfg", "load": "bin/yolov2-tiny-voc.weights", "threshold": 0.5}
+    tfnet = TFNet(options)
+    imgcv = cv2.imread(file_name)
+    result = tfnet.return_predict(imgcv)
+    return result
--- restore.py
+++ restore.py
@@ -6,7 +6,10 @@ import pandas as pd
 from sklearn.utils import shuffle
 from keras.utils import to_categorical
 from keras.models import model_from_json
-
+import camera
+import cv2
+from PIL import Image
+import numpy as np
 
 # load json and create model
 json_file = open('model.json', 'r')
@@ -31,5 +34,29 @@ y_train = dataset[:,0]
 y_train = to_categorical(y_train)
 score = loaded_model.predict(trainX)
 print(y_train.shape)
-print(y_train[2,:])
-print(score)
+print(y_train[5,:])
+# print(score)
+
+i=0
+result = camera.extract_result_set("test.jpg")
+imgcv = cv2.imread("test.jpg")
+print(result)
+for person in result:
+    topleft_x = person['topleft']['x']
+    topleft_y = person['topleft']['y']
+    bottomright_x = person['bottomright']['x']
+    bottomright_y = person['bottomright']['y']
+    if((topleft_y+imgcv.shape[0]*0.08)>imgcv.shape[0]):
+        topleft_y = int(imgcv.shape[0])
+    else:
+        topleft_y = int(topleft_y - (imgcv.shape[0] * 0.1))
+    bottomright_y = int(bottomright_y - (imgcv.shape[0] * 0.1))
+    img = cv2.rectangle(imgcv, (topleft_x, topleft_y), (bottomright_x, bottomright_y), (0, 255, 0), 5)
+    # crop_img = img[topleft_y:bottomright_y, topleft_x:bottomright_x]
+    crop_img=img
+    cv2.imwrite('temp'+str(i)+'.png', crop_img)
+    img = Image.open('temp'+str(i)+'.png').convert('L')
+    i=i+1
+    img = np.array(img.resize((90, 90))).reshape(1,1,90,90).astype( 'float32' )
+    a = loaded_model.predict(img)
+    print(a)
